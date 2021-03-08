4e60a1667c6328562af5df94dc3eb2a47ceb902e
Steven Ly <steven.ly10213@gmail.com>
2021-03-07 20:35:10 -0700

    completed assignment 2

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ main.remove()
 
 const newHeader = document.getElementById('victory')
 
-newHeader.innerHTML = "<h1>'victory' with 'YOUR-NAME is the champion</h1>"
+newHeader.innerHTML = "<h1>'victory' with 'YOUR-NAME is the champion'</h1>"
